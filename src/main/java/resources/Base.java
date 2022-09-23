cf0a34c1424bdbe062c7f0a7d07c1dfec037fb18
neelchavan <neelchavan9@gmail.com>
2022-09-23 21:31:41 +0530

    fixing issues

--- src/main/java/resources/Base.java
+++ src/main/java/resources/Base.java
@@ -31,10 +31,11 @@ public class Base {
 			// to remove unwanted chrome browser warnings use
 			// "webdriver.chrome.silentOutput".
 			System.setProperty("webdriver.chrome.silentOutput", "true");
+//			System.setProperty("webdriver.chrome.driver", "D:\\Neel\\tools\\chromeD\\chromedriver.exe");
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("headless");
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(options);
 		}
 
 		// maximize browser window
@@ -45,10 +46,12 @@ public class Base {
 	}
 
 	public String getScreenshotPath(String methodName, WebDriver driver) throws IOException {
+		String timestamp = String.valueOf(java.time.LocalDateTime.now()).replace(':', '-');
 		// this step stores screenshot into virtual location
 		File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		// move the scrShot from virtual location and store it into desired folder.
-		String destination = System.getProperty("user.dir") + "\\reports\\" + "At_" + methodName + "_.png";
+		String destination = System.getProperty("user.dir") + "\\reports\\" + "At_" + methodName + "_" + timestamp
+				+ "_.png";
 		// copy file at destination
 		FileUtils.copyFile(source, new File(destination));
 		return destination;
